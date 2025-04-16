bdb7327473035078e27ddfc5931aa687644eb7a0
Park-M-S <mmmg0306@g.shingu.ac.kr>
2025-04-16 11:20:58 +0900

    Delete hospital_main directory

--- hospital_main/src/main/java/com/hospital/dao/HospitalDAO.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.dao;
-
-import java.util.List;
-import com.hospital.model.Hospital;
-
-public interface HospitalDAO {
-    List<Hospital> getAllHospitals(); // 병원 위치 전부 가져오기
-}
--- hospital_main/src/main/java/com/hospital/service/HospitalService.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.service;
-
-import com.hospital.model.Hospital;
-import java.util.List;
-
-public interface HospitalService {
-    List<Hospital> getHospitals();
-}
